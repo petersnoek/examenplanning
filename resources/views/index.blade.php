80b3c66877a74cc308e7e13eb17b630976e4e849
HecticKaluha <stefanoverhoeve44@gmail.com>
2018-08-13 21:10:41 +0200

    added oneUI
    installed oneUI theme, setup masterpage consisting of different sections/blade files.
    added some content

---
+++ resources/views/index.blade.php
@@ -0,0 +1,59 @@
+@extends ('layouts.master')
+
+@section ('content')
+    <div class="bg-primary-dark-op">
+        <section class="content content-full content-boxed overflow-hidden">
+            <!-- Section Content -->
+            <div class="push-100-t push-50 text-center">
+                <h1 class="h2 text-white push-10">Examenplanning - Da Vinci College.</h1>
+                <h2 class="h5 text-white-op">Een examenplanningstool voor docent, bedrijf en student.</h2>
+            </div>
+            <!-- END Section Content -->
+        </section>
+    </div>
+    <!-- END Hero Content -->
+
+    <!-- Content -->
+    <div class="bg-white">
+        <section class="content content-boxed">
+            <!-- Section Content -->
+            <h2 class="push-50-t push-50 text-left">Afspraken maken</h2>
+            <p class="height-fix">
+                <img src="images/Full_image_placeholder.jpg" class="landing-image-left" alt="placeholder">
+                Gebruik Examenplanning om gemakkelijk afspraken en examens in te plannen. Maak gemakkelijk een nieuwe gebeurtenis aan en voeg verschillende genodigden toe. De genodigden krijgen een notificatie van de afspraak!
+            </p>
+            <!-- END Section Content -->
+        </section>
+    </div>
+    <!-- END Content -->
+
+    <!-- Content 2 -->
+    <div class="bg-gray-lighter">
+        <section class="content content-boxed">
+            <!-- Section Content -->
+            <h2 class="push-50-t push-50 text-right">Afspraken wijzigen</h2>
+            <!-- END Section Content -->
+        </section>
+    </div>
+    <!-- END Content 2 -->
+
+    <!-- Content 3 -->
+    <div class="bg-white">
+        <section class="content content-boxed">
+            <!-- Section Content -->
+            <h2 class="push-50-t push-50 text-left">Afspraken inzien</h2>
+            <!-- END Section Content -->
+        </section>
+    </div>
+    <!-- END Content 3 -->
+
+    <!-- Content 4 -->
+    <div class="bg-gray-lighter">
+        <section class="content content-boxed">
+            <!-- Section Content -->
+            <h2 class="push-50-t push-50 text-right">Persoonlijke agenda</h2>
+            <!-- END Section Content -->
+        </section>
+    </div>
+    <!-- END Content 4 -->
+@endsection
