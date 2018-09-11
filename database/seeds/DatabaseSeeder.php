fce70c0772489d7c1009b91c906bc4ed8cbbc3d0
Peter Snoek <petersnoek@davinci.nl>
2018-09-11 17:32:31 +0200

    updated userseeder to create 10 records with random names

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,29 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
+    protected $toTruncate = ['users'];
+
     /**
      * Seed the application's database.
      *
      * @return void
      */
+
     public function run()
     {
+        Model::unguard();
+        foreach($this->toTruncate as $table) {
+            DB::table($table)->truncate();
+            $this->command->info("Truncated table: " . $table);
+
+        }
+
         $this->call(UserSeeder::class);
+
+        Model::reguard();
     }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,19 +12,40 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        if (Schema::hasTable('users2') == false) {
+        if (Schema::hasTable('users') == false) {
             $this->command->warn("Seeding Users failed; table 'users' doesn't exist in database...");
             return;
         }
 
         $faker = Faker::create('nl_NL');
 
+        DB::table('users')->insert([
+            'name' => 'Peter Snoek',
+            'email' => 'psnoek@davinci.nl',
+            'password' => bcrypt('Studentje1'),
+        ]);
+        $this->command->info("Seeded user Peter Snoek (psnoek@davinci.nl) with password 'Studentje1'");
+
+
+        DB::table('users')->insert([
+            'name' => 'Developer',
+            'email' => 'dev@dev.com',
+            'password' => bcrypt('password'),
+        ]);
+        $this->command->info("Seeded user Developer (dev@dev.com) with password 'password'");
+
+
         foreach(range(1,10) as $index) {
+            $n = $faker->firstName() . ' ' . $faker->lastName() ;
+            $e = '99'.$faker->randomNumber(6).'@mydavinci.nl';
+            $p = 'password';
             DB::table('users')->insert([
-                'name' => str_random(10),
-                'email' => str_random(10).'@gmail.com',
-                'password' => bcrypt('password'),
+                'name' => $n,
+                'email' => $e,
+                'password' => bcrypt($p),
             ]);
+            $this->command->info("Seeded user " . $n . " (" . $e . ") with password '" . $p . "'");
         }
+
     }
 }
