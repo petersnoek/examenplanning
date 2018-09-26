16d68b3d98907da070b36154530e0901dda6ea55
SJW <sverhoeve@davinci.nl>
2018-09-26 10:02:27 +0200

    RemarkSEeder done

---
+++ database/seeds/RemarkSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use Faker\Factory as Faker;
+
+class RemarkSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (Schema::hasTable('remarks') == false) {
+            $this->command->warn("Seeding remarks failed; table 'remarks' doesn't exist in database...");
+            return;
+        }
+
+        $amount = 52;
+        $faker = Faker::create('nl_NL');
+        $users = User::inRandomOrder()->get();
+
+        foreach(range(1, $amount) as $index)
+        {
+            DB::table('remarks')->insert([
+                'body' => $faker->sentence,
+                'user_id' => $users->pop()->id,
+                'created_at' => Carbon::now(),
+            ]);
+        }
+        $this->command->info('Seeded ' . $amount . ' remarks');
+    }
+}
